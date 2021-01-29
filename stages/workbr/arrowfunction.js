7ec0693fd795c1acfd1827fc6e6bbe6fe12538c4
Danylo <danillazorenko12@gmail.com>
2021-01-29 15:23:29 +0200

    add js file with arrow function

---
+++ stages/workbr/arrowfunction.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let num = 20;
+
+function showFirstMassage(text) {
+    console.log(text);
+    let num = 10;
+    console.log(num);
+
+};
+
+showFirstMassage("Hellow world");
+console.log(num);
+
+function calc(a, b) {
+    return ( a + b );
+}
+
+console.log(calc(4 ,3));
+console.log(calc(5 ,6));
+console.log(calc(10 ,8));
